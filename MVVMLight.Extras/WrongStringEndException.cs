8318479b040f057ed249ea3a9b45febe7c72ce01
Dmitry <stormbringer-s@mail.ru>
2015-06-24 16:40:30 +0400

    Update WrongStringEndException.cs

--- MVVMLight.Extras/WrongStringEndException.cs
+++ MVVMLight.Extras/WrongStringEndException.cs
@@ -2,7 +2,15 @@ using System;
 
 namespace MVVMLight.Extras
 {
+    [Serializable]
     public class WrongStringEndException : Exception
     {
+        public WrongStringEndException() { }
+        public WrongStringEndException(string message) : base(message) { }
+        public WrongStringEndException(string message, Exception inner) : base(message, inner) { }
+        protected WrongStringEndException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
     }
 }

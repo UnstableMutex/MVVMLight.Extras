82c2c36ef2cd958c97f31c6fdb516b1f8917dd99
Dmitry <stormbringer-s@mail.ru>
2015-08-11 12:14:03 +0400

    Create SimpleCommand.cs

---
+++ MVVMLight.Extras/SimpleCommand.cs
@@ -0,0 +1,34 @@
+  class SimpleCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canexecute;
+
+        public SimpleCommand(Action execute, Func<bool> canexecute)
+        {
+            _execute = execute;
+            _canexecute = canexecute;
+        }
+
+        public SimpleCommand(Action execute)
+            : this(execute, () => true)
+        {
+
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _canexecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, EventArgs.Empty);
+        }
+    }
